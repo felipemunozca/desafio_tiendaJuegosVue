131c1cb12531e738487ef03f8dfb2ea3ca5f6160
Felipe Muñoz <analista.felipe.m@gmail.com>
2023-07-02 01:17:37 -0400

    Se completa el desafio.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Tienda Bit-Game</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
+
+    <ListadoJuegos :games="games" @eliminarJuego="eliminarJuegoPadre" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import ListadoJuegos from '@/components/ListadoJuegos.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ListadoJuegos,
+  },
+  data() {
+    return {
+      games: ["Arena of valor", "Pokemon Unite", "Donkey Kong", "Heroes of the storm", "Valorant"]
+    }
+  },
+  methods: {
+    eliminarJuegoPadre(id) {
+      this.games.splice(id, 1)
+    }
+  },
 }
 </script>
 
@@ -21,8 +40,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/ListadoJuegos.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container mt-5">
+        <div class="bg-info-subtle py-2 px-4">
+            <h2>¡Bienvenido!</h2>
+            <hr>
+            <p>A continuación encontraras una lista de los juegos que prefieres</p>
+        </div>
+        <div class="m-auto col-8 mt-5">
+            <table class="table table-dark table-bordered border-white text-center">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Juego</th>
+                        <th scope="col">Eliminar Juego de la lista</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(juego, index) in games" :key="index">
+                        <th scope="row">{{index}}</th>
+                        <td>{{juego}}</td>
+                        <td><button @click="eliminarJuegoHijo(index)" class="btn btn-danger">Eliminar</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'juegos-comp',
+    props: {
+        games: {
+            type: Array,
+            required: true,
+        }
+    },
+    data: function(){
+        return {}
+    },
+    // computed: {},
+    methods: {
+        eliminarJuegoHijo(index) {
+            this.$emit('eliminarJuego', index)
+        }
+    }
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    
+</style>
